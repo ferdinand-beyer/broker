12514f8608ef1e177523118c94f93cab682f0a55
Ferdinand Beyer <ferdinand@wefarm.com>
2022-02-13 20:09:56 +0100

    Improve documentation

--- README.md
+++ README.md
@@ -13,6 +13,85 @@ Simple asynchronous in-process event broker for Clojure.
 
 Available from [Clojars][clojars].
 
+
+## Usage
+
+Start a broker and publish a message:
+
+```clojure
+(require '[com.fbeyer.broker :as broker])
+
+(def broker (broker/start))
+
+(broker/publish! broker {:key :broker-started
+                         :broker broker})
+```
+
+Messages are partitioned by topic.  The topic of a message is determined by
+a `:topic-fn` which can be passed to `start`.  When no `:topic-fn` is
+configured, messages' topic will be grouped by their `:key` key.
+
+```clojure
+(def broker (broker/start {:topic-fn :key}))
+```
+
+To subscribe to messages, use `subscribe` and `subscribe-all`:
+
+```clojure
+(defn notify-hello [msg]
+  (println "Hello," (:name msg)))
+
+(broker/subscribe broker :hello notify-hello)
+
+(broker/publish! broker {:key :hello
+                         :name "Broker!"})
+; Hello, Broker!
+
+(broker/subscribe-all broker #(println "Observed:" %))
+
+(broker/publish! broker {:key :hello
+                         :name "All!"})
+; Observed: {:key :hello, :name All!}
+; Hello, All!
+
+(broker/publish! broker {:key :goodbye})
+; Observed: {:key :goodbye}
+```
+
+Under the hood, `broker` uses [`core.async`][core.async].  Instead of
+functions, you can also subscribe with async channels:
+
+```clojure
+(require '[clojure.core.async :as async])
+
+(def ch (async/chan))
+
+(broker/subscribe broker :goodbye ch)
+```
+
+You can stop receiving messages with `unsubscribe` and release all
+subscriptions with `unsubscribe-all`:
+
+```clojure
+;; Unsubscribe a channel from a topic:
+(broker/unsubscribe broker :goodbye ch)
+
+;; Unsubscribe a function from all topics:
+(broker/unsubscribe broker notify-hello)
+
+;; Remove all subscriptions:
+(broker/unsubscribe-all broker)
+```
+
+You can stop the router with `stop!`:
+
+```clojure
+(broker/stop! broker)
+
+;; This has no effect:
+(broker/publish! broker {:key :more}) ; => false
+```
+
 ## License
 
 Distributed under the [MIT License].  
--- src/com/fbeyer/broker.clj
+++ src/com/fbeyer/broker.clj
@@ -4,39 +4,38 @@
             [clojure.core.async.impl.protocols :as async-protocols]))
 
 (defn start
-  "Starts a new broker.
-
+  "Starts a broker.
    Supported options:
-     :topic-fn - function used to determine the topic of an incoming message
-                 (default: :key).
-     :error-fn - when a subscribing function throws an exception, this function
-                 will be called with two arguments: the exception and a map with
-                 keys :broker, :fn, and :msg.  By default, exceptions are
-                 silently ignored."
-  [& [{:keys [topic-fn error-fn]
-       :or   {topic-fn :key}}]]
-  ;; TODO: Buffers for the source channel and pub channels?
-  (let [ch   (async/chan)
-        mult (async/mult ch)]
-    {::ch   ch
-     ::mult mult
-     ::pub  (-> (async/tap mult (async/chan))
-                (async/pub topic-fn))
-     ::subs   (atom {})
-     ::taps   (atom #{})
-     ::fn-chs (atom {})
-     ::error-fn error-fn}))
+   * `:topic-fn` - function used to determine the topic of an incoming message
+     for [[subscribe]]; default: `:key`.
+   * `:error-fn` - when a subscribing function throws an exception, this function
+     will be called with two arguments: the exception and a map with keys
+     `:broker`, `:fn`, and `:msg`.  With no `:error-fn` (default), exceptions are
+     silently ignored."
+  ([] (start nil))
+  ([opts]
+   ;; TODO: Buffers for the source channel and pub channels?
+   (let [ch   (async/chan)
+         mult (async/mult ch)]
+     {::ch       ch
+      ::mult     mult
+      ::pub      (-> (async/tap mult (async/chan))
+                     (async/pub (:topic-fn opts :key)))
+      ::subs     (atom {})
+      ::taps     (atom #{})
+      ::fn-chs   (atom {})
+      ::error-fn (:error-fn opts)})))
 
 (defn stop!
-  "Stops the broker, closing all internal async channels.  After that, the
-   broker will no longer accept messages.  Any priorly published messages will
-   still be delivered.  Returns nil."
+  "Stops the broker, closing all internal async channels.
+   After that, the broker will no longer accept messages.  Any priorly
+   published messages will still be delivered.  Returns `nil`."
   [broker]
   (async/close! (::ch broker)))
 
 (defn publish!
-  "Publishes a message.  All subscribers will be notified asynchronously.
-   Returns true unless broker is stopped."
+  "Publishes `msg` to subscribers, who will be notified asynchronously.
+   Returns `true` unless `broker` is stopped."
   [broker msg]
   (async/put! (::ch broker) msg))
 
@@ -70,30 +69,32 @@
     :else (throw (ex-info "expected a function or channel" {:actual x}))))
 
 (defn subscribe
-  "Subscribes a function or channel to a topic.  If fn-or-ch is already
-   subscribed to this topic, this is a no-op.
+  "Subscribes a function or channel to a topic.
+   If `fn-or-ch` is already subscribed to this topic, this is a no-op.
 
-   When fn-or-ch is a function, it will be run in an async (go ...) block and
-   should not block.  Exceptions will be reported to the broker's :error-fn
-   (see start)."
-  [{::keys [pub subs] :as broker} topic fn-or-ch]
-  (let [ch (->chan broker fn-or-ch)]
+   When `fn-or-ch` is a function, it will be run in an async `(go ...)` block
+   and should not block.  Exceptions will be reported to the broker's
+   `:error-fn` (see [[start]])."
+  [broker topic fn-or-ch]
+  (let [{::keys [pub subs]} broker
+        ch (->chan broker fn-or-ch)]
     (async/sub pub topic ch)
     (swap! subs update ch (fnil conj #{}) topic)
     nil))
 
 (defn subscribe-all
   "Subscribes a function or channel to all messages published to the broker.
-   When fn-or-ch is already subscribed to all messages, this is a no-op.
+   When `fn-or-ch` is already subscribed to all messages, this is a no-op.
 
-   When fn-or-ch is also subscribed explicitly to a topic, it will receive
+   When `fn-or-ch` is also subscribed explicitly to a topic, it will receive
    messages twice.
 
-   When fn-or-ch is a function, it will be run in an async (go ...) block and
-   should not block.  Exceptions will be reported to the broker's :error-fn
-   (see start)."
-  [{::keys [mult taps] :as broker} fn-or-ch]
-  (let [ch (->chan broker fn-or-ch)]
+   When `fn-or-ch` is a function, it will be run in an async `(go ...)` block
+   and should not block.  Exceptions will be reported to the broker's
+   `:error-fn` (see [[start]])."
+  [broker fn-or-ch]
+  (let [{::keys [mult taps]} broker
+        ch (->chan broker fn-or-ch)]
     (async/tap mult ch)
     (swap! taps conj ch)
     nil))
@@ -131,22 +132,26 @@
   (reset! taps #{}))
 
 (defn unsubscribe
-  "Unsubscribes a function or channel.  If a topic is given, unsubscribes only
-   from this topic.  Otherwise, unsubscribes from all messages.  If fn-or-ch
-   is not a subscriber, this is a no-op.  Returns nil."
-  ([{::keys [fn-chs] :as broker} fn-or-ch]
-   (when-let [ch (get-sub-chan fn-or-ch fn-chs)]
+  "Unsubscribes a function or channel.
+   If a `topic` is given, unsubscribes only from this topic.  Otherwise,
+   unsubscribes from all topics, i.e. clears all subscriptions from
+   [[subscribe]] and [[subscribe-all]].
+
+   If `fn-or-ch` is not a subscriber, this is a no-op.  Returns `nil`."
+  ([broker fn-or-ch]
+   (when-let [ch (get-sub-chan fn-or-ch (:fn-chs broker))]
      (unsub-chan-from-all broker ch)
      (untap-chan broker ch)
      nil))
-  ([{::keys [fn-chs] :as broker} topic fn-or-ch]
-   (when-let [ch (get-sub-chan fn-or-ch fn-chs)]
+  ([broker topic fn-or-ch]
+   (when-let [ch (get-sub-chan fn-or-ch (:fn-chs broker))]
      (unsub-chan broker topic ch)
      nil)))
 
 (defn unsubscribe-all
-  "Unsubscribes all subscribers.  When a topic is given, only subscribers to
-   the given topic will be unsubscribed.  Returns nil."
+  "Unsubscribes all subscribers.
+   When a `topic` is given, only subscribers to the given topic will be
+   unsubscribed.  Returns `nil`."
   ([broker]
    (unsub-all broker)
    (untap-all broker)
