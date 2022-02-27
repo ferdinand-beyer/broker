2a73eb33b32945a350fc4566a0c55938707652e3
Ferdinand Beyer <ferdinand@wefarm.com>
2022-02-27 13:16:55 +0100

    Use custom dispatcher
    ...instead of mults and pubs.

--- src/com/fbeyer/broker.clj
+++ src/com/fbeyer/broker.clj
@@ -1,14 +1,18 @@
 (ns com.fbeyer.broker
-  "A simple in-process message/event broker for Clojure."
   (:require [clojure.core.async :as async]
             [clojure.core.async.impl.protocols :as async-protocols]
             [clojure.set :as set]))
 
-(def ^:private empty-subdb
-  {:next-id 0
-   :id      {}
-   :target  {}
-   :topic   {}})
+;; --- Store ---
+
+(def ^:private empty-store
+  {:next-id  0
+   :data     {}
+   :by-topic {}
+   :by-recp  {}})
+
+(defn- get-sub [store id]
+  (get-in store [:data id]))
 
 (def ^:private set-conj (fnil conj #{}))
 
@@ -21,152 +25,64 @@
       (assoc m k s)
       (dissoc m k))))
 
-(defn- update-topics [subdb topics f & args]
-  (update subdb :topic (fn [tmap]
-                         (reduce (fn [m k] (apply f m k args))
-                                 tmap topics))))
+(defn- update-topics [store topics f & args]
+  (update store :by-topic
+          (fn [tmap]
+            (reduce (fn [m k] (apply f m k args))
+                    tmap topics))))
 
-(defn- add-sub [subdb {:keys [target topics] :as sub}]
-  (let [id (:next-id subdb)]
-    (-> subdb
+(defn- add-sub [store {:keys [recp topics] :as sub}]
+  (let [id (:next-id store)]
+    (-> store
         (update :next-id inc)
-        (assoc-in [:id id] (assoc sub :id id))
-        (update-in [:target target] set-conj id)
+        (assoc-in [:data id] (assoc sub :id id))
+        (update-in [:by-recp recp] set-conj id)
         (update-topics topics set-assoc id))))
 
-(defn- add-sub-topics [subdb id topics]
-  (-> subdb
-      (update-in [:id id :topics] set/union topics)
+(defn- add-sub-topics [store id topics]
+  (-> store
+      (update-in [:data id :topics] set/union topics)
       (update-topics topics set-assoc id)))
 
-(defn- remove-sub [subdb id]
-  (let [{:keys [target topics]} (get-in subdb [:id id])]
-    (-> subdb
-        (update :id dissoc id)
-        (update :target set-dissoc target id)
+(defn- remove-sub [store id]
+  (let [{:keys [recp topics]} (get-in store [:data id])]
+    (-> store
+        (update :data dissoc id)
+        (update :by-recp set-dissoc recp id)
         (update-topics topics set-dissoc id))))
 
-(defn- remove-sub-topics [subdb id topics]
-  (-> subdb
-      (update-in [:id id :topics] set/difference topics)
+(defn- remove-sub-topics [store id topics]
+  (-> store
+      (update-in [:data id :topics] set/difference topics)
       (update-topics topics set-dissoc id)))
 
-(defn- all-subs [subdb]
-  (-> subdb :id vals))
+(defn- all-subs [store]
+  (-> store :data vals))
 
-(defn- target-subs [subdb target]
-  (->> (get-in subdb [:target target])
-       (map #(get-in subdb [:id %]))))
+(defn- recp-subs [store recp]
+  (->> (get-in store [:by-recp recp])
+       (map (partial get-sub store))))
 
-(defn- target-topics [subdb target]
-  (->> (target-subs subdb target)
+(defn- recp-topics [store recp]
+  (->> (recp-subs store recp)
        (mapcat :topics)
        (into #{})))
 
-(defn- topic-subs [subdb topic]
-  (->> (get-in subdb [:topic topic])
-       (map #(get-in subdb [:id %]))))
-
-(defn- find-compatible [subdb target opts]
-  (->> (target-subs subdb target)
+(defn- find-recp-sub [store recp opts]
+  (->> (recp-subs store recp)
        (filter #(= opts (:opts %)))
        first))
 
-(defn- thread-uncaught-exc-handler
-  [e _]
-  (let [thread (Thread/currentThread)]
-    (-> (.getUncaughtExceptionHandler thread)
-        (.uncaughtException thread e)))
-  nil)
+(defn- topic-subs [store topic]
+  (->> (get-in store [:by-topic topic])
+       (map (partial get-sub store))))
 
-(defn start
-  "Starts a broker.
+(defn- matching-subs [store topics]
+  (->> (mapcat #(get-in store [:by-topic %]) topics)
+       (into #{})
+       (map (partial get-sub store))))
 
-   Supported options:
-   * `:topic-fn` - function used to determine the topic of an incoming message
-     for [[subscribe]].  Defaults to `first`, expecting messages to be vectors
-     `[topic payload...]`.
-   * `:xform` - a transducer to transform/filter messages as they are published.
-     Should not throw exceptions and must not produce `nil` messages.
-   * `:buf-or-n` - `core.async` buffer to use for the publish channel.  Defaults
-     to a large fixed-size buffer (`1024`).
-   * `:buf-fn` - function to create `core.async` buffers for subscribing
-     functions.  By default, uses small fixed-size buffers (`8`).
-   * `:error-fn` - when a subscribing function throws an exception, this function
-     will be called with two arguments: the exception and a map with keys
-     `:broker`, `:fn`, and `:msg`.  With no `:error-fn` (default), exceptions are
-     passed to the current thread's `UncaughtExceptionHandler`.
-   * `:exec` - default subscription execution type -- see [[subscribe]]."
-  ([] (start nil))
-  ([opts]
-   (let [source (async/chan (:buf-or-n opts 1024)
-                            (:xform opts))
-         mult   (async/mult source)]
-     {::source   source
-      ::stop     (async/chan)
-      ::mult     mult
-      ::pub      (-> (async/tap mult (async/chan)) ; unbuffered - ok?
-                     (async/pub (:topic-fn opts first)))
-      ::subdb    (atom empty-subdb)
-      ::buf-fn   (:buf-fn opts (constantly 8))
-      ::error-fn (:error-fn opts thread-uncaught-exc-handler)
-      ::exec     (:exec opts :sequential)})))
-
-(defn- exec-chans [{::keys [subdb]}]
-  (keep :exec-ch (all-subs @subdb)))
-
-(defn stop-chan
-  "Returns a channel that will close when the broker stops and all
-   pending messages are processed by the subscribers.  Can be used
-   to block for a graceful shutdown."
-  [broker]
-  (async/go
-    (async/<! (::stop broker))
-    (let [ch (async/merge (exec-chans broker))]
-      (loop []
-        (when (async/<! ch)
-          (recur))))))
-
-(defn stop!
-  "Stops the broker, closing all internal async channels.
-   After that, the broker will no longer accept messages.  Any priorly
-   published messages will still be delivered.  Can be called multiple
-   times.  Returns `nil`."
-  [broker]
-  (async/close! (::source broker))
-  (async/close! (::stop broker)))
-
-(defn shutdown!
-  "Stops the broker and waits for all messages to be processed, or `timeout-ms`
-   to pass.  Returns `true` when successfully stopped, or `false` on timeout."
-  ([broker] (shutdown! broker 60000))
-  ([broker timeout-ms]
-   (stop! broker)
-   (async/alt!!
-     (stop-chan broker) true
-     (async/timeout timeout-ms) false)))
-
-(defn publish!
-  "Publishes `msg` to subscribers, who will be notified asynchronously.
-   `msg` must not be `nil`.  Returns `true` unless `broker` is stopped.
-
-   Under high load, this will block the caller to respect back-pressure.
-   As such, this should not be called from `(go ...)` blocks.
-   When blocking is not desired, use `core.async/put!` on the channel
-   returned by [[publish-chan]], or install a windowed buffer using the
-   `:buf-or-n` option of [[start]] to drop messages under high load."
-  [broker msg]
-  (async/>!! (::source broker) msg))
-
-(defn publish-chan
-  "Returns the channel used for publishing messages to the broker.
-
-   Intended for publishing from a `(go ...)` block or advanced usage
-   such as bulk publishing / piping into the broker.
-
-   The caller must not close the returned channel."
-  [broker]
-  (::source broker))
+;; --- Executors ---
 
 (defn- drop-all [rf]
   (fn
@@ -206,17 +122,17 @@
 
 ;; Run in a core.async pipeline.
 (defmethod -make-exec :pipeline [_ ch f {:keys [parallel]
-                                             :or {parallel 1}}]
+                                         :or {parallel 1}}]
   (async/pipeline parallel (null-chan) (keep f) ch))
 
 ;; Run in a core.async blocking pipeline.
 (defmethod -make-exec :blocking [_ ch f {:keys [parallel]
-                                             :or {parallel 1}}]
+                                         :or {parallel 1}}]
   (async/pipeline-blocking parallel (null-chan) (keep f) ch))
 
 ;; Run in a core.async async pipeline.
 (defmethod -make-exec :async [_ ch f {:keys [parallel]
-                                          :or {parallel 1}}]
+                                      :or {parallel 1}}]
   (let [af (fn [msg out]
              (f msg #(async/close! out)))]
     (async/pipeline-async parallel (null-chan) af ch)))
@@ -239,52 +155,211 @@
 (defn- write-port? [x]
   (satisfies? async-protocols/WritePort x))
 
-(defn- make-sub [broker topics f opts]
-  (let [sub {:topics topics
-             :target f
-             :opts   opts}]
-    (if (write-port? f)
+;; --- Control ---
+
+(defn- make-sub [broker recp opts topics]
+  (let [sub {:recp   recp
+             :opts   opts
+             :topics topics}]
+    (if (write-port? recp)
       (do (assert (nil? opts))
-          (assoc sub :ch f))
+          (assoc sub :ch recp))
       (let [ch      (make-chan broker opts)
-            exec-ch (make-exec broker ch f opts)]
+            exec-ch (make-exec broker ch recp opts)]
         (assoc sub :ch ch :exec-ch exec-ch)))))
 
-(defn- sub-ch [{::keys [mult pub]} topics ch]
-  (doseq [topic topics]
-    (if (= ::all topic)
-      (async/tap mult ch)
-      (async/sub pub topic ch))))
-
-(defn- unsub-ch [{::keys [mult pub]} topics ch]
-  (doseq [topic topics]
-    (if (= ::all topic)
-      (async/untap mult ch)
-      (async/unsub pub topic ch))))
-
-(defn- close-sub! [{:keys [ch exec-ch]}]
-  (when exec-ch
-    (async/close! ch)))
-
 (defn- delete-sub!
-  [{::keys [subdb] :as broker} {:keys [id topics ch] :as sub}]
-  (unsub-ch broker topics ch)
-  (close-sub! sub)
-  (swap! subdb remove-sub id))
+  [{::keys [store]} {:keys [id ch exec-ch]}]
+  (when exec-ch
+    (async/close! ch))
+  (swap! store remove-sub id))
 
 (defn- unsub-topics
-  [{::keys [subdb] :as broker} {:keys [id ch] :as sub} topics]
+  [{::keys [store] :as broker} {:keys [id] :as sub} topics]
   (let [sub-topics   (:topics sub)
         unsub-topics (set/intersection sub-topics topics)]
     (if (= unsub-topics sub-topics)
       (delete-sub! broker sub)
-      (do (unsub-ch broker unsub-topics ch)
-          (swap! subdb remove-sub-topics id unsub-topics)))))
+      (swap! store remove-sub-topics id unsub-topics))))
 
-(defn- unsub-target-topics
-  [{::keys [subdb] :as broker} topics f]
-  (doseq [sub (target-subs @subdb f)]
+(defn- unsub-recp-topics
+  [{::keys [store] :as broker} recp topics]
+  (doseq [sub (recp-subs @store recp)]
     (unsub-topics broker sub topics)))
+
+(defmulti ^:private handle-cmd (fn [_broker cmd] (first cmd)))
+
+(defmethod handle-cmd :subscribe
+  [{::keys [store] :as broker} [_ recp opts topics]]
+  (if-let [{:keys [id] :as sub} (find-recp-sub @store recp opts)]
+    (let [new-topics (set/difference topics (:topics sub))]
+      (when (seq new-topics)
+        (unsub-recp-topics broker recp new-topics)
+        (swap! store add-sub-topics id new-topics)))
+    (let [sub (make-sub broker recp opts topics)]
+      (unsub-recp-topics broker recp topics)
+      (swap! store add-sub sub))))
+
+(defmethod handle-cmd :remove-closed-sub
+  [{::keys [store]} [_ id]]
+  (swap! store remove-sub id))
+
+(defmethod handle-cmd :unsubscribe-recp
+  [{::keys [store] :as broker} [_ recp]]
+  (let [topics (recp-topics @store recp)]
+    (unsub-recp-topics broker recp topics)))
+
+(defmethod handle-cmd :unsubscribe-recp-topics
+  [broker [_ recp topics]]
+  (unsub-recp-topics broker recp topics))
+
+(defmethod handle-cmd :unsubscribe-all
+  [{::keys [store]} _]
+  (doseq [{:keys [ch exec-ch]} (all-subs @store)]
+    (when exec-ch
+      (async/close! ch)))
+  (reset! store empty-store))
+
+(defmethod handle-cmd :unsubscribe-topics
+  [{::keys [store] :as broker} [_ topics]]
+  (doseq [topic topics
+          sub   (topic-subs @store topic)]
+    (unsub-topics broker sub topics)))
+
+(defmethod handle-cmd :sync [_ [_ out]]
+  (async/put! out true))
+
+(defn- run-control [{::keys [ctrl-ch] :as broker}]
+  (async/go-loop []
+    (when-some [cmd (async/<! ctrl-ch)]
+      (handle-cmd broker cmd)
+      (recur))))
+
+(defn- send-cmd [{::keys [ctrl-ch]} cmd]
+  (let [ch (async/chan 1)]
+    (and (async/>!! ctrl-ch cmd)
+         (async/>!! ctrl-ch [:sync ch])
+         (async/<!! ch))))
+
+;; --- Dispatch ---
+
+(defn- run-dispatch [{::keys [dispatch-ch ctrl-ch topic-fn store]}]
+  (let [todo-cnt  (atom nil)
+        done      (async/chan 1)
+        delivered (fn [_] (when (zero? (swap! todo-cnt dec))
+                            (async/put! done true)))]
+    (async/go-loop []
+      (if-some [msg (async/<! dispatch-ch)]
+        (let [topic (topic-fn msg)
+              subs  (matching-subs @store [::all topic])]
+          (when (seq subs)
+            (reset! todo-cnt (count subs))
+            (doseq [{:keys [id ch]} subs]
+              (when-not (async/put! ch msg delivered)
+                (async/>! ctrl-ch [:remove-closed-sub id])))
+            (async/<! done))
+          (recur))
+        (doseq [{:keys [ch]} (all-subs @store)]
+          (async/close! ch))))))
+
+;; --- Broker ---
+
+(defn- thread-uncaught-exc-handler
+  [e _]
+  (let [thread (Thread/currentThread)]
+    (-> (.getUncaughtExceptionHandler thread)
+        (.uncaughtException thread e)))
+  nil)
+
+(defn start
+  "Starts a broker.
+
+   Supported options:
+   * `:topic-fn` - function used to determine the topic of an incoming message
+     for [[subscribe]].  Defaults to `first`, expecting messages to be vectors
+     `[topic payload...]`.
+   * `:xform` - a transducer to transform/filter messages as they are published.
+     Should not throw exceptions and must not produce `nil` messages.
+   * `:buf-or-n` - `core.async` buffer to use for the publish channel.  Defaults
+     to a large fixed-size buffer (`1024`).
+   * `:buf-fn` - function to create `core.async` buffers for subscribing
+     functions.  By default, uses small fixed-size buffers (`8`).
+   * `:error-fn` - when a subscribing function throws an exception, this function
+     will be called with two arguments: the exception and a map with keys
+     `:broker`, `:fn`, and `:msg`.  With no `:error-fn` (default), exceptions are
+     passed to the current thread's `UncaughtExceptionHandler`.
+   * `:exec` - default subscription execution type -- see [[subscribe]]."
+  ([] (start nil))
+  ([opts]
+   (let [broker {::dispatch-ch (async/chan (:buf-or-n opts 1024)
+                                           (:xform opts))
+                 ::ctrl-ch     (async/chan)
+                 ::stop-ch     (async/chan)
+                 ::store       (atom empty-store)
+                 ::topic-fn    (:topic-fn opts first)
+                 ::buf-fn      (:buf-fn opts (constantly 8))
+                 ::error-fn    (:error-fn opts thread-uncaught-exc-handler)
+                 ::exec        (:exec opts :sequential)}]
+     ;; TODO: Save those channels (e.g. instead of stop channel)?
+     (run-control broker)
+     (run-dispatch broker)
+     broker)))
+
+(defn- exec-chans [{::keys [store]}]
+  (keep :exec-ch (all-subs @store)))
+
+(defn stop-chan
+  "Returns a channel that will close when the broker stops and all
+   pending messages are processed by the subscribers.  Can be used
+   to block for a graceful shutdown."
+  [broker]
+  (async/go
+    (async/<! (::stop-ch broker))
+    (let [ch (async/merge (exec-chans broker))]
+      (loop []
+        (when (async/<! ch)
+          (recur))))))
+
+(defn stop!
+  "Stops the broker, closing all internal async channels.
+   After that, the broker will no longer accept messages.  Any priorly
+   published messages will still be delivered.  Can be called multiple
+   times.  Returns `nil`."
+  [broker]
+  (doseq [k [::dispatch-ch ::ctrl-ch ::stop-ch]]
+    (async/close! (broker k))))
+
+(defn shutdown!
+  "Stops the broker and waits for all messages to be processed, or `timeout-ms`
+   to pass.  Returns `true` when successfully stopped, or `false` on timeout."
+  ([broker] (shutdown! broker 60000))
+  ([broker timeout-ms]
+   (stop! broker)
+   (async/alt!!
+     (stop-chan broker) true
+     (async/timeout timeout-ms) false)))
+
+(defn publish!
+  "Publishes `msg` to subscribers, who will be notified asynchronously.
+   `msg` must not be `nil`.  Returns `true` unless `broker` is stopped.
+
+   Under high load, this will block the caller to respect back-pressure.
+   As such, this should not be called from `(go ...)` blocks.
+   When blocking is not desired, use `core.async/put!` on the channel
+   returned by [[publish-chan]], or install a windowed buffer using the
+   `:buf-or-n` option of [[start]] to drop messages under high load."
+  [broker msg]
+  (async/>!! (::dispatch-ch broker) msg))
+
+(defn publish-chan
+  "Returns the channel used for publishing messages to the broker.
+
+   Intended for publishing from a `(go ...)` block or advanced usage
+   such as bulk publishing / piping into the broker.
+
+   The caller must not close the returned channel."
+  [broker]
+  (::dispatch-ch broker))
 
 (defn- topics->set [topics]
   (cond
@@ -327,19 +402,7 @@
      (subscribe broker nil topic-or-f f-or-opts)
      (subscribe broker topic-or-f f-or-opts nil)))
   ([broker topic f opts]
-   (let [topics (topics->set topic)
-         subdb  (::subdb broker)]
-     (if-let [{:keys [id ch] :as sub} (find-compatible @subdb f opts)]
-       (let [new-topics (set/difference topics (:topic sub))]
-         (when (seq new-topics)
-           (unsub-target-topics broker new-topics f)
-           (swap! subdb add-sub-topics id new-topics)
-           (sub-ch broker new-topics ch)))
-       (let [{:keys [ch] :as sub} (make-sub broker topics f opts)]
-         (unsub-target-topics broker topics f)
-         (swap! subdb add-sub sub)
-         (sub-ch broker topics ch))))
-   nil))
+   (send-cmd broker [:subscribe f opts (topics->set topic)])))
 
 (defn unsubscribe
   "Unsubscribes a function or channel.
@@ -349,56 +412,15 @@
 
    If `f` is not a subscriber, this is a no-op.  Returns `nil`."
   ([broker f]
-   (let [subdb  (::subdb broker)
-         topics (target-topics @subdb f)]
-     (unsub-target-topics broker topics f)))
+   (send-cmd broker [:unsubscribe-recp f]))
   ([broker topic f]
-   (unsub-target-topics broker (topics->set topic) f)))
+   (send-cmd broker [:unsubscribe-recp-topics f (topics->set topic)])))
 
 (defn unsubscribe-all
   "Unsubscribes all subscribers.
    When a `topic` is given, only subscribers to the given topic will be
    unsubscribed.  Returns `nil`."
   ([broker]
-   (let [{::keys [mult pub subdb]} broker]
-     (async/unsub-all pub)
-     (doseq [{:keys [ch topics] :as sub} (all-subs @subdb)]
-       (when (contains? topics ::all)
-         (async/untap mult ch))
-       (close-sub! sub))
-     (reset! subdb empty-subdb)
-     nil))
+   (send-cmd broker [:unsubscribe-all]))
   ([broker topic]
-   (let [subdb  (::subdb broker)
-         topics (topics->set topic)]
-     (doseq [topic topics
-             sub   (topic-subs @subdb topic)]
-       (unsub-topics broker sub topics)))))
-
-(comment
-  ;; TODO: Reproducing example:
-  (dotimes [n 100]
-    (let [start  (async/chan)
-          pub    (async/pub start first)
-          end    (async/chan)
-          _      (async/sub pub :a end)
-          _      (async/sub pub :b end)
-          recv   (atom #{})
-          done   (async/go-loop []
-                   (when-let [v (async/<! end)]
-                     (swap! recv conj v)
-                     (recur)))
-          sent   [[:a 1] [:a 2] [:b 3]]]
-      (doseq [v sent]
-        (async/>!! start v))
-      (async/close! start)
-      (async/<!! done)
-      (let [exp (set sent)
-            act (set @recv)]
-        (when (not= exp act)
-          (println "No." n)
-          (println "Expected:" exp)
-          (println "Actual:  " act)
-          (assert false)))))
-
-  )
+   (send-cmd broker [:unsubscribe-topics (topics->set topic)])))
